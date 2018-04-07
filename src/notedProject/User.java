39fab2e4b38f5513c0047e37a1e570f48a2dc49a
Leon Chen <chenchaoyang@chenhaodeMacBook-Pro.local>
2018-04-07 13:20:20 -0700

    CH-User

---
+++ src/notedProject/User.java
@@ -0,0 +1,14 @@
+package notedProject;
+
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+import com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper;
+import com.sun.xml.internal.bind.v2.runtime.Name;
+
+public class User {
+	private Name name;
+	public class Name{
+		private String firstname;
+		private String lastname;
+	}
+	
+}
