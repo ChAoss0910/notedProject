8b6ec9c2ec159b3cd7c20fe38831a981e13f14fc
Leon Chen <chenchaoyang@chenhaodeMacBook-Pro.local>
2018-04-07 13:59:18 -0700

    CH-Question

--- src/notedProject/Question.java
+++ src/notedProject/Question.java
@@ -1,5 +1,27 @@
 package notedProject;
 
+import java.util.ArrayList;
+
 public class Question {
 	private String title;
+	private ArrayList<String> options = new ArrayList<String>();
+	private int correctAnwser;
+	public Question(String title) {
+		 this.title = title;
+	 }
+	public String getTitle() {
+		return this.title;
+	}
+	public ArrayList<String> getOptions() {
+		return this.options;
+	}
+	public int getAnswer() {
+		return this.correctAnwser;
+	}
+	public void addOption(String m) {
+		options.add(m);
+	}
+	public void remove(int pos) {
+		options.remove(pos-1);
+	}
 }
