1b1135dfd63b33f8ca79bc28a550488816645827
chenchaoyang <chenchaoyang@10.9.35.136>
2018-04-07 13:30:43 -0700

    # WARNING: head commit changed in the meantime
    (CH)-User

--- src/notedProject/User.java
+++ src/notedProject/User.java
@@ -1,14 +1,38 @@
 package notedProject;
 
-import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
-import com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper;
-import com.sun.xml.internal.bind.v2.runtime.Name;
+import java.io.Serializable;
 
-public class User {
+public class User implements Serializable{
 	private Name name;
-	public class Name{
+	public class Name implements Serializable{
 		private String firstname;
 		private String lastname;
-	}
 	
+		public String getFirst() {
+			return this.firstname;
+		}
+		public String getLast() {
+			return this.lastname;
+		}
+	}
+	private String username;
+	private String email;
+	private String password;
+	
+	public String getFirstName() {
+		return this.name.getFirst();
+	}
+	public String getLastName() {
+		return this.name.getLast();
+	}	
+	public String getUsername() {
+		return this.username;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+
 }
